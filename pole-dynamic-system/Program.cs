43d81845b73fac7ca360f81448d6c9a6a60fb263
Davide Lettieri <davide.lettieri@gmail.com>
2025-10-18 17:39:18 +0200

    review: apply fixes and suggestions

--- pole-dynamic-system/Program.cs
+++ pole-dynamic-system/Program.cs
@@ -1,43 +1,42 @@
 ﻿// port of http://incompleteideas.net/book/code/pole.c
 // copyright of the authors, see the link above for details.
 
-const int N_BOXES = 162;
-const int ALPHA = 1000;
-const float BETA = 0.5f;
-const float GAMMA = 0.95f;
-const float LAMBDAw = 0.9f;
-const float LAMBDAv = 0.8f;
+const int NumBoxes = 162;
+const int Alpha = 1000;
+const float Beta = 0.5f;
+const float Gamma = 0.95f;
+const float LambdaW = 0.9f;
+const float LambdaV = 0.8f;
 
-const int MAX_FAILURES = 100;
-const int MAX_STEPS = 100000;
+const int MaxFailures = 100;
+const int MaxSteps = 100000;
 
 float x, x_dot; // position and velocity of the cart
 float theta, theta_dot; // angle and angular velocity of the pole
 
-float[] w = new float[N_BOXES];
-float[] v = new float[N_BOXES];
-float[] e = new float[N_BOXES];
-float[] xBar = new float[N_BOXES];
+float[] w = new float[NumBoxes];
+float[] v = new float[NumBoxes];
+float[] e = new float[NumBoxes];
+float[] xBar = new float[NumBoxes];
 
 
 float p, oldP, r;
 int steps = 0, failures = 0;
 
-for (var i = 0; i < N_BOXES; i++)
-{
-    w[i] = v[i] = xBar[i] = e[i] = 0.0f;
-}
-
 x = x_dot = theta = theta_dot = 0.0f;
 int box = getBox(x, x_dot, theta, theta_dot);
 
-while (steps++ < MAX_STEPS && failures < MAX_FAILURES)
+Console.WriteLine("Starting pole balancing. Select an integer number to seed the random instance:");
+int a = int.Parse(Console.ReadLine() ?? "0");
+var rand = new Random(a);
+
+while (steps++ < MaxSteps && failures < MaxFailures)
 {
     bool failed;
-    var y = Random.Shared.NextSingle() < ProbPushRight(w[box]) ? 1 : 0;
+    var y = rand.NextSingle() < probPushRight(w[box]) ? 1 : 0;
 
-    e[box] += (1 - LAMBDAw) * (y - 0.5f);
-    xBar[box] += (1 - LAMBDAv);
+    e[box] += (1 - LambdaW) * (y - 0.5f);
+    xBar[box] += (1 - LambdaV);
 
     oldP = v[box];
     (x, x_dot, theta, theta_dot) = CartPole(y, x, x_dot, theta, theta_dot);
@@ -63,12 +62,12 @@ while (steps++ < MAX_STEPS && failures < MAX_FAILURES)
         p = v[box];
     }
 
-    var rHat = r + GAMMA * p - oldP;
+    var rHat = r + Gamma * p - oldP;
 
-    for (var i = 0; i < N_BOXES; i++)
+    for (var i = 0; i < NumBoxes; i++)
     {
-        w[i] += ALPHA * rHat * e[i];
-        v[i] += BETA * rHat * xBar[i];
+        w[i] += Alpha * rHat * e[i];
+        v[i] += Beta * rHat * xBar[i];
 
         if (failed)
         {
@@ -77,51 +76,50 @@ while (steps++ < MAX_STEPS && failures < MAX_FAILURES)
         }
         else
         {
-            e[i] *= LAMBDAw;
-            xBar[i] *= LAMBDAv;
+            e[i] *= LambdaW;
+            xBar[i] *= LambdaV;
         }
     }
 }
 
-if (failures == MAX_FAILURES)
+if (failures == MaxFailures)
 {
-    Console.WriteLine("Pole not balanced. Stopping after {0} failures.", failures);
+    Console.WriteLine($"Pole not balanced. Stopping after {failures} failures.");
 }
 else
 {
-    Console.WriteLine("Pole balanced successfully for at least {0} steps", steps);
+    Console.WriteLine($"Pole balanced successfully for at least {steps} steps");
 }
 
 static (float x, float x_dot, float theta, float theta_dot) CartPole(int action, float x, float x_dot, float theta, float theta_dot)
 {
-    const float gravity = 9.8f;
-    const float masscart = 1.0f;
-    const float masspole = 0.1f;
-    const float total_mass = masspole + masscart;
-    const float length = 0.5f; // actually half the pole's length
-    const float polemass_length = masspole * length;
-    const float force_mag = 10.0f;
-    const float tau = 0.02f; // seconds between state updates
-    const float fourthirds = 4.0f / 3.0f;
+    const float Gravity = 9.8f;
+    const float MassCart = 1.0f;
+    const float MassPole = 0.1f;
+    const float TotalMass = MassPole + MassCart;
+    const float Length = 0.5f; // actually half the pole's length
+    const float PoleMassLength = MassPole * Length;
+    const float ForceMag = 10.0f;
+    const float Tau = 0.02f; // seconds between state updates
+    const float FourThirds = 4.0f / 3.0f;
 
-    float force = (action == 1) ? force_mag : -force_mag;
+    float force = (action == 1) ? ForceMag : -ForceMag;
     float costheta = MathF.Cos(theta);
     float sintheta = MathF.Sin(theta);
-    float temp = (force + polemass_length * theta_dot * theta_dot * sintheta) / total_mass;
-    float thetaacc = (gravity * sintheta - costheta * temp) / (length * (fourthirds - masspole * costheta * costheta / total_mass));
-    float xacc = temp - polemass_length * thetaacc * costheta / total_mass;
-    x += tau * x_dot;
-    x_dot += tau * xacc;
-    theta += tau * theta_dot;
-    theta_dot += tau * thetaacc;
+    float temp = (force + PoleMassLength * theta_dot * theta_dot * sintheta) / TotalMass;
+    float thetaacc = (Gravity * sintheta - costheta * temp) / (Length * (FourThirds - MassPole * costheta * costheta / TotalMass));
+    float xacc = temp - PoleMassLength * thetaacc * costheta / TotalMass;
+    x += Tau * x_dot;
+    x_dot += Tau * xacc;
+    theta += Tau * theta_dot;
+    theta_dot += Tau * thetaacc;
     return (x, x_dot, theta, theta_dot);
 }
 
-static float ProbPushRight(float s)
+static float probPushRight(float s)
 {
-    return 1.0f / (1.0f + MathF.Exp(Math.Max(-50.0f, Math.Min(s, 50.0f))));
+    return 1.0f / (1.0f + MathF.Exp(-MathF.Max(-50.0f, MathF.Min(s, 50.0f))));
 }
-
 
 static int getBox(float x, float x_dot, float theta, float theta_dot)
 {
@@ -130,38 +128,63 @@ static int getBox(float x, float x_dot, float theta, float theta_dot)
     const float twelve_degrees = 0.2094384f;
     const float fifty_degrees = 0.87266f;
 
-    int box = 0;
-
     if (x < -2.4f || x > 2.4f || theta < -twelve_degrees || theta > twelve_degrees)
     {
         return -1;
     }
 
-    if (x >= -0.8f && x < 0.8f)
+    int box;
+    if (x < -0.8f)
+    {
+        box = 0;
+    }
+    else if (x < 0.8f)
+    {
         box = 1;
+    }
     else
+    {
         box = 2;
+    }
 
-    if (x_dot >= -0.5f && x_dot < 0.5f)
-        box += 3;
-    else
+    if (x_dot >= 0.5f)
+    {
         box += 6;
+    }
+    else if (x_dot >= -0.5f)
+    {
+        box += 3;
+    }
 
-    if (theta >= -six_degrees && theta < -one_degree)
-        box += 9;
-    else if (theta < 0.0f)
-        box += 18;
-    else if (theta < one_degree)
-        box += 27;
-    else if (theta < six_degrees)
-        box += 36;
-    else
+    if (theta >= six_degrees)
+    {
         box += 45;
+    }
+    else if (theta >= one_degree)
+    {
+        box += 36;
+    }
+    else if (theta >= 0.0f)
+    {
+        box += 27;
+    }
+    else if (theta >= -one_degree)
+    {
+        box += 18;
+    }
+    else if (theta >= -six_degrees)
+    {
+        box += 9;
+    }
 
-    if (theta_dot >= -fifty_degrees && theta_dot < fifty_degrees)
-        box += 54;
-    else
+    if (theta_dot >= fifty_degrees)
+    {
         box += 108;
+    }
+    else if (theta_dot >= -fifty_degrees)
+    {
+        box += 54;
+    }
 
     return box;
 }
